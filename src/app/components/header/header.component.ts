b571881db463692fe1111f8bac4d14f1d5fc6e88
Sam C <samc@designmynight.com>
2019-12-29 17:17:08 +0000

    refactor: header now shows the current page title as a sub nav

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../../services/auth.service';
+import { Router, NavigationEnd } from '@angular/router';
 
 @Component({
     selector: 'app-header',
@@ -10,11 +11,32 @@ export class HeaderComponent implements OnInit {
 
     public isUserLoggedIn: Boolean = false;
     public showDropdown: Boolean = false;
+    public currentUrl: string = '';
 
-    constructor(private _authService: AuthService) {
+    constructor(private _authService: AuthService, private _router: Router) {
         this._authService.userLoggedInChange.subscribe(value => {
             this.isUserLoggedIn = value;
         });
+
+        this._router.events.subscribe((val: any) => {
+            if(val instanceof NavigationEnd) {
+                this.currentUrl = val.url;
+                console.log(this.currentUrl);
+            }
+        });
+    }
+
+    get navigationTitle() {
+        switch(this.currentUrl) {
+            case '/dashboard':
+                return 'Dashboard';
+            case '/manage-inventory':
+                return 'Inventory';
+            case '/manage-chores':
+                return 'Chores';
+            default:
+                return '';
+        }
     }
 
     ngOnInit() {}
